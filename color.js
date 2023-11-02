67f7a9c0369bf914e27abc875323e4b1c5d0ee6a
kateistrans <kateistrans@users.noreply.github.com>
2023-11-02 11:45:27 +0000

    deploy: d369e1ea1e8f032486743a58144dfccb2730740b

---
+++ color.js
@@ -0,0 +1,9 @@
+const RED = "#ff0000";
+const ORANGE = "#ffa500";
+const YELLOW = "#ffff00";
+const GREEN = "#00ff00";
+const BLUE = "#0000ff";
+const PURPLE = "#ff00ff";
+
+const BLACK = "#000000";
+const WHITE = "#ffffff";
